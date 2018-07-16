5845bf1bb54719ef6c54f58f51c333c5abd163c6
unknown <freemudadmin@SH-WJ-ZHANG-PC1.office.freemud.cn>
2018-07-16 14:27:41 +0800

    请求封装

--- utils/util.js
+++ utils/util.js
@@ -13,7 +13,59 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+function request(url, params, success, fail) {
+  this.requestLoading(url, params, "", success, fail)
+}
+// 展示进度条的网络请求
+// url:网络请求的url
+// params:请求参数
+// message:进度条的提示信息
+// success:成功的回调函数
+// fail：失败的回调
+function requestLoading(url, params, message, success, fail) {
+  console.log(params)
+  wx.showNavigationBarLoading()
+  if (message != "") {
+    wx.showLoading({
+      title: message,
+    })
+  }
+  wx.request({
+    url: url,
+    data: params,
+    header: {
+      //'Content-Type': 'application/json'
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    method: 'post',
+    success: function (res) {
+      //console.log(res.data)
+      wx.hideNavigationBarLoading()
+      if (message != "") {
+        wx.hideLoading()
+      }
+      if (res.statusCode == 200) {
+        success(res.data)
+      } else {
+        fail()
+      }
+
+    },
+    fail: function (res) {
+      wx.hideNavigationBarLoading()
+      if (message != "") {
+        wx.hideLoading()
+      }
+      fail()
+    },
+    complete: function (res) {
+
+    },
+  })
+}
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  request: request,
+  requestLoading: requestLoading
 }
